fa899d5f358edbcefcb38c40576ee21b8ac07827
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-11-14 11:29:57 +0100

    Update and Refactor Reason Node Telemetry

--- src/Application/Workflows/ReAct/Nodes/ReasonNode.cs
+++ src/Application/Workflows/ReAct/Nodes/ReasonNode.cs
@@ -4,6 +4,7 @@ using Application.Workflows.ReAct.Dto;
 using Microsoft.Agents.AI.Workflows;
 using Microsoft.Agents.AI.Workflows.Reflection;
 using Microsoft.Extensions.AI;
+using System.Diagnostics;
 
 namespace Application.Workflows.ReAct.Nodes;
 
@@ -15,39 +16,51 @@ public class ReasonNode(IAgent agent) : ReflectingExecutor<ReasonNode>("ReasonNo
     public async ValueTask<ActRequest> HandleAsync(ChatMessage message, IWorkflowContext context,
         CancellationToken cancellationToken = default)
     {
-        using var activity = Telemetry.Start("Reason-[handle]");
+        using var activity = Telemetry.Start("reason_handle_request");
 
-        activity?.SetTag("User", message.Text);
+        activity?.SetTag("react.node", "reason_node");
+
+
+        activity?.SetTag("react.input.message", message.Text);
 
         _messages.Add(message);
-        
+
+        activity?.AddEvent(new ActivityEvent("llm_request_sent"));
+
         var response = await agent.RunAsync(_messages, cancellationToken: cancellationToken);
+
+        activity?.AddEvent(new ActivityEvent("llm_response_received"));
+
 
         var responseMessage = response.Messages.First();
         
         _messages.Add(responseMessage);
 
-        activity?.SetTag("Assistant", response.Messages.First().Text);
-
+        activity?.SetTag("react.output.message", response.Messages.First().Text);
+  
         return new ActRequest(response.Messages.First());
     }
 
     public async ValueTask<ActRequest> HandleAsync(ActObservation actObservation, IWorkflowContext context,
         CancellationToken cancellationToken = new CancellationToken())
     {
-        using var activity = Telemetry.Start("Reason-[observe]");
+        using var activity = Telemetry.Start("reason_handle_observe");
 
-        activity?.SetTag("User", actObservation.Message);
+        activity?.SetTag("react.observation.response_message", actObservation.Message);
 
         var message = new ChatMessage(ChatRole.User, actObservation.Message);
 
         _messages.Add(message);
 
+        activity?.AddEvent(new ActivityEvent("llm_request_sent"));
+
         var response = await agent.RunAsync(_messages, cancellationToken: cancellationToken);
 
         _messages.Add(response.Messages.First());
 
-        activity?.SetTag("Assistant", response.Messages.First().Text);
+        activity?.AddEvent(new ActivityEvent("llm_response_received"));
+
+        activity?.SetTag("react.output.message", response.Messages.First().Text);
 
         return new ActRequest(response.Messages.First());
     }
