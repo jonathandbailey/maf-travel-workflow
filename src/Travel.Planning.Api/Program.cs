9d3a6cd7b72bdd93ee96cd1533db6079b281e073
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-01-15 10:50:49 +0100

    Expose a2a endpoint for workflow

--- src/Travel.Planning.Api/Program.cs
+++ src/Travel.Planning.Api/Program.cs
@@ -1,3 +1,5 @@
+using A2A;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -14,6 +16,40 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+var agentCard = new AgentCard
+{
+    Name = "Travel_Planning",
+    Description = "An agent that plans your travel.",
+    Url = "https://localhost:7286/api/a2a/travel/",
+    Skills =
+    [
+        new AgentSkill { Name = "Search Flights", Description = "Searches for your flights." }
+    ],
+    Version = "1.0"
+};
+
+var taskManager = new TaskManager();
+
+taskManager.OnAgentCardQuery += (args, cancellationToken) => Task.FromResult(agentCard);
+
+taskManager.OnMessageReceived += async (@params, token) =>
+{
+  
+    var message = new AgentMessage()
+    {
+        Role = MessageRole.Agent,
+        MessageId = Guid.NewGuid().ToString(),
+        ContextId = @params.Message.ContextId,
+        Parts = [new TextPart() {
+            Text = "This is the test response"
+        }]
+    };
+
+    return message;
+};
+
+app.MapA2A(taskManager, "/api/a2a/travel");
 
 app.Run();
 
